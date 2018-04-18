35d105407beb21fe0fa54a81690c09a2ac3589a1
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-04-18 10:32:57 +0530

    Created GithubProject class

--- src/main/java/io/yadnyesh/sb2/events/GithubProject.java
+++ src/main/java/io/yadnyesh/sb2/events/GithubProject.java
@@ -1,5 +1,55 @@
 package io.yadnyesh.sb2.events;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class GithubProject {
 
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String orgName;
+	
+	@Column(unique = true)
+	private String repoName;
+	
+	public GithubProject() {
+	}
+	
+	public GithubProject(String orgName, String repoName) {
+		this.orgName = orgName;
+		this.repoName = repoName;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getOrgName() {
+		return orgName;
+	}
+	
+	public void setOrgName(String orgName) {
+		this.orgName = orgName;
+	}
+	
+	public String getRepoName() {
+		return repoName;
+	}
+	
+	public void setRepoName(String repoName) {
+		this.repoName = repoName;
+	}
+	
+	@Override
+	public String toString() {
+		return "GithubProject{" + "id=" + id + ", orgName='" + orgName + '\'' + ", repoName='" + repoName + '\'' + '}';
+	}
 }
