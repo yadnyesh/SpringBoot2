238b5660d18b5756db7f09b5b25fd2ccec67250c
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-04-18 12:23:13 +0530

    Added Actor class

--- src/main/java/io/yadnyesh/sb2/github/Actor.java
+++ src/main/java/io/yadnyesh/sb2/github/Actor.java
@@ -1,5 +1,45 @@
 package io.yadnyesh.sb2.github;
 
-public class Actor {
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+public class Actor {
+	
+	private String login;
+	private String avatarUrl;
+	private String htmlUrl;
+	
+	public Actor(@JsonProperty("login") String login, @JsonProperty("avatar_url") String avatarUrl, @JsonProperty("html_url") String htmlUrl) {
+		this.login = login;
+		this.avatarUrl = avatarUrl;
+		this.htmlUrl = htmlUrl;
+	}
+	
+	public String getLogin() {
+		return login;
+	}
+	
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	
+	public String getAvatarUrl() {
+		return avatarUrl;
+	}
+	
+	public void setAvatarUrl(String avatarUrl) {
+		this.avatarUrl = avatarUrl;
+	}
+	
+	public String getHtmlUrl() {
+		return htmlUrl;
+	}
+	
+	public void setHtmlUrl(String htmlUrl) {
+		this.htmlUrl = htmlUrl;
+	}
+	
+	@Override
+	public String toString() {
+		return "Actor{" + "login='" + login + '\'' + ", avatarUrl='" + avatarUrl + '\'' + ", htmlUrl='" + htmlUrl + '\'' + '}';
+	}
 }
