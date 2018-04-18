81a6634a2b2d009bf173a09a0a87dcd458268dfa
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-04-18 10:39:18 +0530

    Created interface GithubProjectRepository

--- src/main/java/io/yadnyesh/sb2/repository/GithubProjectRepository.java
+++ src/main/java/io/yadnyesh/sb2/repository/GithubProjectRepository.java
@@ -2,8 +2,7 @@ package io.yadnyesh.sb2.repository;
 
 import io.yadnyesh.sb2.events.GithubProject;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
 
-public interface GithubProjectRepository implements PagingAndSortingRepository<GithubProject, Long> {
-
+public interface GithubProjectRepository extends PagingAndSortingRepository<GithubProject, Long> {
+	GithubProject findByRepoName(String repoName);
 }
