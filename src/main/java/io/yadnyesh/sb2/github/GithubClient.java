13d8de75ee8029138dc1f9d1a56f7a5c857d9a27
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-04-19 12:08:47 +0530

    See example for RestTemplateBuilder

--- src/main/java/io/yadnyesh/sb2/github/GithubClient.java
+++ src/main/java/io/yadnyesh/sb2/github/GithubClient.java
@@ -1,6 +1,15 @@
 package io.yadnyesh.sb2.github;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
+@Component
 public class GithubClient {
-
+	public String EVENTS_ISSUES_URL = "https://api.github.com/repos/{owner}/{repo}/issues/events";
+	public final RestTemplate restTemplate;
+	
+	public GithubClient(RestTemplateBuilder restTemplateBuilder) {
+		this.restTemplate = restTemplateBuilder.build();
+	}
 }
